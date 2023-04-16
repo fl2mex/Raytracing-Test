71092ca759492ea5bc5405423b55baea2988de57
fl2mexd <kaidenchrisbird@gmail.com>
2023-04-17 02:39:06 +1000

    Finished One Weekend

--- Raytracing Test/src/hittable_list.h
+++ Raytracing Test/src/hittable_list.h
@@ -1,40 +1,43 @@
-#pragma once
-#include <memory>
-#include <vector>
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
 
 #include "hittable.h"
 
-class hittable_list : public hittable
-{
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class hittable_list : public hittable {
 public:
-	hittable_list() {}
-	hittable_list(std::shared_ptr<hittable> object) { add(object); }
+    hittable_list() {}
+    hittable_list(shared_ptr<hittable> object) { add(object); }
 
-	void clear() { objects.clear(); }
-	void add(std::shared_ptr<hittable> object) { objects.push_back(object); }
+    void clear() { objects.clear(); }
+    void add(shared_ptr<hittable> object) { objects.push_back(object); }
 
-	virtual bool hit(
-		const ray& r, double t_min, double t_max, hit_record& rec) const override;
+    virtual bool hit(
+        const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
 public:
-	std::vector<std::shared_ptr<hittable>> objects;
+    std::vector<shared_ptr<hittable>> objects;
 };
 
-bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
-{
-	hit_record temp_rec;
-	bool hit_anything = false;
-	auto closest_so_far = t_max;
+bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    hit_record temp_rec;
+    bool hit_anything = false;
+    auto closest_so_far = t_max;
 
-	for (const auto& object : objects)
-	{
-		if (object->hit(r, t_min, closest_so_far, temp_rec))
-		{
-			hit_anything = true;
-			closest_so_far = temp_rec.t;
-			rec = temp_rec;
-		}
-	}
+    for (const auto& object : objects) {
+        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
 
-	return hit_anything;
+    return hit_anything;
 }
+
+#endif
--- Raytracing Test/src/shared.h
+++ Raytracing Test/src/shared.h
@@ -1,25 +1,42 @@
-#pragma once
+#ifndef SHARED_H
+#define SHARED_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
 #include <cstdlib>
-#include <random>
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-inline double random_double()
-{
-	return rand() / (RAND_MAX + 1.0);
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
 }
-inline double random_double(double min, double max)
-{
-	return min + (max - min) * random_double();
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
 }
-inline double degrees_to_radians(double degrees)
-{
-	return degrees * pi / 180.0;
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
 }
-inline double clamp(double x, double min, double max)
-{
-	if (x < min) return min;
-	if (x > max) return max;
-	return x;
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
+
+#endif
