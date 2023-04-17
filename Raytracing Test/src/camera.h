211f36a027535fb861bd0bc915294093df694f34
fl2mexd <kaidenchrisbird@gmail.com>
2023-04-18 00:51:15 +1000

    Finished The Next Week

--- Raytracing Test/src/camera.h
+++ Raytracing Test/src/camera.h
@@ -6,7 +6,8 @@
 class camera {
 public:
     camera(point3 lookfrom, point3 lookat, vec3 vup,
-    double vfov, double aspect_ratio, double aperture, double focus_dist) {
+    double vfov, double aspect_ratio, double aperture,
+    double focus_dist, double _time0 = 0, double _time1 = 0) {
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
@@ -22,13 +23,19 @@ public:
         lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
 
         lens_radius = aperture / 2;
+        time0 = _time0;
+        time1 = _time1;
     }
 
     ray get_ray(double s, double t) const {
         vec3 rd = lens_radius * random_in_unit_disk();
         vec3 offset = u * rd.x() + v * rd.y();
 
-        return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset);
+        return ray(
+            origin + offset,
+            lower_left_corner + s * horizontal + t * vertical - origin - offset,
+            random_double(time0, time1)
+        );
     }
 
 private:
@@ -38,5 +45,6 @@ private:
     vec3 vertical;
     vec3 u, v, w;
     double lens_radius;
+    double time0, time1;
 };
 #endif
--- Raytracing Test/src/material.h
+++ Raytracing Test/src/material.h
@@ -4,11 +4,15 @@
 #include "shared.h"
 #include "hittable.h"
 #include "ray.h"
+#include "texture.h"
 
 struct hit_record;
 
 class material {
 public:
+    virtual color emitted(double u, double v, const point3& p) const {
+        return color(0, 0, 0);
+    }
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
     ) const = 0;
@@ -16,7 +20,8 @@ public:
 
 class lambertian : public material {
 public:
-    lambertian(const color& a) : albedo(a) {}
+    lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+    lambertian(shared_ptr<texture> a) : albedo(a) {}
 
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
@@ -24,13 +29,13 @@ public:
         auto scatter_direction = rec.normal + random_unit_vector();
         if (scatter_direction.near_zero())
             scatter_direction = rec.normal;
-        scattered = ray(rec.p, scatter_direction);
-        attenuation = albedo;
+        scattered = ray(rec.p, scatter_direction, r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
         return true;
     }
 
 public:
-    color albedo;
+    shared_ptr<texture> albedo;
 };
 
 class metal : public material {
@@ -41,7 +46,7 @@ public:
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
     ) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere(), r_in.time());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
@@ -73,7 +78,7 @@ public:
         else
             direction = refract(unit_direction, rec.normal, refraction_ratio);
 
-        scattered = ray(rec.p, direction);
+        scattered = ray(rec.p, direction, r_in.time());
         return true;
     }
 
@@ -87,6 +92,42 @@ private:
         r0 = r0 * r0;
         return r0 + (1 - r0) * pow((1 - cosine), 5);
     }
+};
+
+class diffuse_light : public material {
+public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const override {
+        return false;
+    }
+
+    virtual color emitted(double u, double v, const point3& p) const override {
+        return emit->value(u, v, p);
+    }
+
+public:
+    shared_ptr<texture> emit;
+};
+
+class isotropic : public material {
+public:
+    isotropic(color c) : albedo(make_shared<solid_color>(c)) {}
+    isotropic(shared_ptr<texture> a) : albedo(a) {}
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const override {
+        scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+
+public:
+    shared_ptr<texture> albedo;
 };
 
 #endif
