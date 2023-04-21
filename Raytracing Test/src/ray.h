cd5993aeb1694a84015c739b22e286345341a686
fl2mexd <kaidenchrisbird@gmail.com>
2023-04-21 19:28:15 +1000

    Multithreading, TODO: Fix PDF

--- Raytracing Test/src/ray.h
+++ Raytracing Test/src/ray.h
@@ -1,25 +1,20 @@
-#ifndef RAY_H
-#define RAY_H
+#pragma once
 #include "vec3.h"
 
-class ray {
+class ray
+{
 public:
-    ray() {}
-    ray(const point3& origin, const vec3& direction, double time = 0.0)
-        : orig(origin), dir(direction), tm(time)
-    {}
+	ray() {}
+	ray(const point3& origin, const vec3& direction, double time = 0.0)
+		: orig(origin), dir(direction), tm(time) {}
 
-    point3 origin() const { return orig; }
-    vec3 direction() const { return dir; }
-    double time() const { return tm; }
+	point3 origin() const { return orig; }
+	vec3 direction() const { return dir; }
+	double time() const { return tm; }
 
-    point3 at(double t) const {
-        return orig + t * dir;
-    }
+	point3 at(double t) const { return orig + t * dir; }
 
-public:
-    point3 orig;
-    vec3 dir;
-    double tm;
+	point3 orig;
+	vec3 dir;
+	double tm;
 };
-#endif
--- Raytracing Test/src/shared.h
+++ Raytracing Test/src/shared.h
@@ -1,6 +1,4 @@
-#ifndef SHARED_H
-#define SHARED_H
-
+#pragma once
 #include <cmath>
 #include <limits>
 #include <memory>
@@ -10,29 +8,17 @@ using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
-
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
+inline double degrees_to_radians(double degrees) { return degrees * pi / 180.0; }
+inline double random_double() { return rand() / (RAND_MAX + 1.0); }
+inline double random_double(double min, double max) { return min + (max - min) * random_double(); }
+inline int random_int(int min, int max) { return static_cast<int>(random_double(min, max + 1)); }
 
-inline double degrees_to_radians(double degrees) {
-    return degrees * pi / 180.0;
+inline double clamp(double x, double min, double max)
+{
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
 }
-inline double random_double() {
-    // Returns a random real in [0,1).
-    return rand() / (RAND_MAX + 1.0);
-}
-inline double random_double(double min, double max) {
-    // Returns a random real in [min,max).
-    return min + (max - min) * random_double();
-}
-inline double clamp(double x, double min, double max) {
-    if (x < min) return min;
-    if (x > max) return max;
-    return x;
-}
-inline int random_int(int min, int max) {
-    // Returns a random integer in [min,max].
-    return static_cast<int>(random_double(min, max + 1));
-}
-#endif
